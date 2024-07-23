6e01a6c04dead3f624888fca024cd2c6ac8e2eb9
Olga <olgaalaquas@gmail.com>
2024-07-23 13:11:40 +0200

    created template html and css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,46 @@
-import logo from '../images/adalab.png';
-import '../scss/App.scss';
+import logo from "../images/adalab.png";
+import "../scss/App.scss";
 
 function App() {
   return (
     <>
       <header className="header">
-        <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
-          <img className="header__companyLogo" src={logo} alt="Logo proyectos molones" />
+        <a
+          className="header__brand"
+          href="./"
+          title="Haz click para volver a la página inicial"
+        >
+          <img
+            className="header__companyLogo"
+            src={logo}
+            alt="Logo proyectos molones"
+          />
           <h1 className="header__title">Proyectos molones</h1>
         </a>
         <img className="logoSponsor" src={logo} alt="Logo Adalab" />
       </header>
       <section className="hero">
         <h2 className="title">Proyectos molones</h2>
-        <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
-        <a className="button--link" href="./">Ver proyectos</a>
+        <p className="hero__text">
+          Escaparate en línea para recoger ideas a través de la tecnología
+        </p>
+        <a className="button--link" href="./">
+          Ver proyectos
+        </a>
       </section>
 
       <section className="preview">
         <div className="projectImage"></div>
         <article className="card">
-          <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal project card</span></h2>
+          <h2 className="card__projectTitle">
+            <span className="card__projectTitle--text">
+              Personal project card
+            </span>
+          </h2>
 
           <div className="card__author">
             <div className="card__authorPhoto"></div>
-            <p className="card__job">
-              Full stack Developer
-            </p>
+            <p className="card__job">Full stack Developer</p>
             <h3 className="card__name">Emmelie Bjôrklund</h3>
           </div>
 
@@ -34,55 +48,132 @@ function App() {
             <h3 className="card__name">Elegant Workspace</h3>
             <p className="card__slogan">Diseños Exclusivos</p>
             <h3 className="card__descriptionTitle">Product description</h3>
-            <p className="card__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione</p>
+            <p className="card__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla,
+              quos? Itaque, molestias eveniet laudantium adipisci vitae ratione
+            </p>
 
             <div className="card__technicalInfo">
               <p className="card__technologies">React JS - HTML - CSS</p>
 
-              <a className="icon icon__www" href="#" title="Haz click para ver el proyecto online">
+              <a
+                className="icon icon__www"
+                href="#"
+                title="Haz click para ver el proyecto online"
+              >
                 Web link
               </a>
-              <a className="icon icon__github" href="#" title="Haz click para ver el código del proyecto">
+              <a
+                className="icon icon__github"
+                href="#"
+                title="Haz click para ver el código del proyecto"
+              >
                 GitHub link
               </a>
             </div>
           </div>
         </article>
       </section>
-      <form class="addForm">
-        <h2 class="title">Información</h2>
-        <fieldset class="addForm__group">
-          <legend class="addForm__title">Cuéntanos sobre el proyecto</legend>
-          <input class="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto">
-            <input class="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan">
-              <div class="addForm__2col">
-                <input class="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio">
-                  <input class="addForm__input" type="url" name="demo" id="demo" placeholder="Demo">
-                  </div>
-                  <input class="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías">
-                    <textarea class="addForm__input" type="text" name="desc" id="desc" placeholder="Descripción" rows="5"></textarea>
-                  </fieldset>
+      <form className="addhtmlForm">
+        <h2 className="title">InhtmlFormación</h2>
+        <fieldset className="addhtmlForm__group">
+          <legend className="addhtmlForm__title">
+            Cuéntanos sobre el proyecto
+          </legend>
+          <input
+            className="addhtmlForm__input"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Nombre del proyecto"
+          />
+          <input
+            className="addhtmlForm__input"
+            type="text"
+            name="slogan"
+            id="slogan"
+            placeholder="Slogan"
+          />
+          <div className="addhtmlForm__2col">
+            <input
+              className="addhtmlForm__input"
+              type="url"
+              name="repo"
+              id="repo"
+              placeholder="Repositorio"
+            />
+            <input
+              className="addhtmlForm__input"
+              type="url"
+              name="demo"
+              id="demo"
+              placeholder="Demo"
+            />
+          </div>
+          <input
+            className="addhtmlForm__input"
+            type="text"
+            name="technologies"
+            id="technologies"
+            placeholder="Tecnologías"
+          />
+          <textarea
+            className="addhtmlForm__input"
+            type="text"
+            name="desc"
+            id="desc"
+            placeholder="Descripción"
+            rows="5"
+          ></textarea>
+        </fieldset>
 
-                  <fieldset class="addForm__group">
-                    <legend class="addForm__title">Cuéntanos sobre la autora</legend>
-                    <input class="addForm__input" type="text" name="autor" id="autor" placeholder="Nombre">
-                      <input class="addForm__input" type="text" name="job" id="job" placeholder="Trabajo">
-                      </fieldset>
+        <fieldset className="addhtmlForm__group">
+          <legend className="addhtmlForm__title">
+            Cuéntanos sobre la autora
+          </legend>
+          <input
+            className="addhtmlForm__input"
+            type="text"
+            name="autor"
+            id="autor"
+            placeholder="Nombre"
+          />
+          <input
+            className="addhtmlForm__input"
+            type="text"
+            name="job"
+            id="job"
+            placeholder="Trabajo"
+          />
+        </fieldset>
 
-                      <fieldset class="addForm__group--upload">
-                        <label for="image" class="button">Subir foto del proyecto</label>
-                        <input class="addForm__hidden" type="file" name="image" id="image">
-                          <label for="photo" class="button">Subir foto de la autora</label>
-                          <input class="addForm__hidden" type="file" name="photo" id="photo">
-                            <button class="button--large">Guardar proyecto</button>
-                          </fieldset>
+        <fieldset className="addhtmlForm__group--upload" />
+        <label htmlFor="image" className="button">
+          Subir foto del proyecto
+        </label>
+        <input
+          className="addhtmlForm__hidden"
+          type="file"
+          name="image"
+          id="image"
+        />
+        <label htmlFor="photo" className="button">
+          Subir foto de la autora
+        </label>
+        <input
+          className="addhtmlForm__hidden"
+          type="file"
+          name="photo"
+          id="photo"
+        />
+        <button className="button--large">Guardar proyecto</button>
+      </form>
 
-                        </form>
-                        <footer className="footer">
-                          <img className="logoSponsor" src={logo} alt="Logo Adalab" />
-                        </footer>
-                      </>
-                      )
+      <footer className="footer">
+        <img className="logoSponsor" src={logo} alt="Logo Adalab" />
+      </footer>
+    </>
+  );
 }
 
-                      export default App
+export default App;
