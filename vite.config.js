86c81f7141a1d85d4fd1bc05430561c6d2b57e55
griseldagomez <grinoe123@gmail.com>
2024-07-31 14:34:15 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true,
+  },
   base: "/project-promo-Y-module-3-team-2/"
 })
